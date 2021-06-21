0dd067d53b12d6616d357e3052e7faae6b245cda
daniaalnahdi <alnahdi.d@husky.neu.edu>
2021-06-22 02:24:44 +0300

    base setup and routes

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB,
+});
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('App is listening on port ' + listener.address().port);
+});
+
+app.get('/meetings', (req, res) => {
+  db.query('SELECT * FROM meeting', (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.get('/meetings/:id', (req, res) => {
+  db.query(
+    'SELECT * FROM meeting WHERE meeting_id = ?',
+    req.params.id,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+  
